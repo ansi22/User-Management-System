54c131eb511bf3be4d4211d92c8ee3bfb2ee6760
Ritika Verma <iansi@Ritikas-MacBook-Air.local>
2023-06-30 19:11:18 +0530

    commit-3: postmapping for sending data in db

--- src/main/java/com/ums/fullstack/controller/UserController.java
+++ src/main/java/com/ums/fullstack/controller/UserController.java
@@ -1,2 +1,19 @@
-package com.ums.fullstack.controller;public class UserController {
+package com.ums.fullstack.controller;
+
+import com.ums.fullstack.model.User;
+import com.ums.fullstack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
 }
